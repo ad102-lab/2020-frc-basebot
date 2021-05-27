daa1a5f56b642f66fcbb037f2d6da3cb0d53e0f1
Coy Chanders <Coy@Chanders.com>
2021-05-26 20:09:19 -0600

    commented out examples that are needed to compile

--- src/main/java/org/team1619/state/modelogic/TeleopModeLogic.java
+++ src/main/java/org/team1619/state/modelogic/TeleopModeLogic.java
@@ -39,8 +39,8 @@ public class TeleopModeLogic extends AbstractModeLogic {
 		switch (name) {
 //			case "st_example_zero":
 //				return !fSharedInputValues.getBoolean("ipb_example_has_been_zeroed");
-			case "st_example":
-				return fSharedInputValues.getBooleanRisingEdge("ipb_operator_right_trigger");
+//			case "st_example":
+//				return fSharedInputValues.getBooleanRisingEdge("ipb_operator_right_trigger");
 			default:
 				return false;
 		}
